5ea4deb0a08b91a7326c158b2211725ecb3dd195
anthonyassaf <62218418+anthonyassaf@users.noreply.github.com>
2021-02-07 15:42:06 +0200

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
